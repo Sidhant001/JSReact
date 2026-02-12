f1af3edaa30a6fe46c80cfca12a973ba0117027d
Sidhant001 <143825824+Sidhant001@users.noreply.github.com>
2026-02-12 12:09:48 +0530

    Some basic changes

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from "react"
 function App() {
-  const [color,setColor] = useState("rgba(122, 118, 118, 0.47)")
+  const [color,setColor] = useState("rgba(153, 206, 172, 0.51)")
   return (
     <div className="w-full h-screen "
     style={{backgroundColor:color}}
@@ -9,37 +9,44 @@ function App() {
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
           onClick={()=>setColor("Red")}
-          className="outline-2 px-5 py-1.5 rounded-full shadow-lg"
+          className="outline-2 px-5 py-1.5 rounded-full shadow-lg
+          hover:scale-108 hover:brightness-150"
           style={{backgroundColor:"red"}}
           >Red</button>
           <button
           onClick={()=>setColor("Blue")}
-          className="outline-2 px-5 py-1.5 rounded-full shadow-lg"
+          className="outline-2 px-5 py-1.5 rounded-full shadow-lg
+          hover:scale-108 hover:brightness-130"
           style={{backgroundColor:"lightblue"}}
           >Blue</button>
           <button
           onClick={()=>setColor("green")}
-          className="outline-2 px-5 py-1.5 rounded-full shadow-lg"
+          className="outline-2 px-5 py-1.5 rounded-full shadow-lg
+          hover:scale-108 hover:brightness-120"
           style={{backgroundColor:"lightgreen"}}
           >Green</button>
           <button
           onClick={()=>setColor("yellow")}
-          className="outline-2 px-5 py-1.5 rounded-full shadow-lg"
+          className="outline-2 px-5 py-1.5 rounded-full shadow-lg
+          hover:scale-108 hover:brightness-130"
           style={{backgroundColor:"yellow"}}
           >Yellow</button>
           <button
           onClick={()=>setColor("black")}
-          className="outline-2 px-5 py-1.5 rounded-full shadow-lg"
+          className="outline-2 px-5 py-1.5 rounded-full shadow-lg
+          hover:scale-108 hover:brightness-130"
           style={{backgroundColor:"grey"}}
           >Black</button>
           <button
           onClick={()=>setColor("Orange")}
-          className="outline-2 px-5 py-1.5 rounded-full shadow-lg"
+          className="outline-2 px-5 py-1.5 rounded-full shadow-lg
+          hover:scale-108 hover:brightness-130"
           style={{backgroundColor:"Orange"}}
           >Orange</button>
           <button
           onClick={()=>setColor("Pink")}
-          className="outline-2 px-5 py-1.5 rounded-full shadow-lg"
+          className="outline-2 px-5 py-1.5 rounded-full shadow-lg
+          hover:scale-108 hover:brightness-110"
           style={{backgroundColor:"pink"}}
           >Pink</button>
         </div>
